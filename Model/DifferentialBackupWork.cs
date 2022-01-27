c1ccfc05f7fcf72d206aac3cf7d619682d95911e
IWillBecomeAGod <75016647+IWillBecomeAGod@users.noreply.github.com>
2022-01-27 11:03:33 +0100

    copy a folder from a location to another location

--- Model/DifferentialBackupWork.cs
+++ Model/DifferentialBackupWork.cs
@@ -8,7 +8,41 @@ namespace EasySave.Model
     {
         public override void ExecuteBackup()
         {
-            throw new NotImplementedException();
+
+            string destFile;
+
+            if (!System.IO.Directory.Exists(targetDirectory))
+            {
+                System.IO.Directory.CreateDirectory(targetDirectory);
+            }
+
+            // To copy all the files in one directory to another directory. 
+            // Get the files in the source folder. (To recursively iterate through 
+            // all subfolders under the current directory, see 
+            // "How to: Iterate Through a Directory Tree.")
+            // Note: Check for target path was performed previously 
+            //       in this code example. 
+            if (System.IO.Directory.Exists(sourceDirectory))
+            {
+                string[] files = System.IO.Directory.GetFiles(sourceDirectory);
+
+                // Copy the files and overwrite destination files if they already exist. 
+                foreach (string s in files)
+                {
+                    // Use static Path methods to extract only the file name from the path.
+                    name = System.IO.Path.GetFileName(s);
+                    destFile = System.IO.Path.Combine(targetDirectory, name);
+                    System.IO.File.Copy(s, destFile, true);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Source path does not exist!");
+            }
+
+            // Keep console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
