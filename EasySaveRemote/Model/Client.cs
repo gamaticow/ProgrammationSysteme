7998048d40e7de16d82d71d56b129bac1bf6dd25
romaricDembele <romadembele@gmail.com>
2022-02-21 14:25:00 +0100

    Setup Remote EasySave global structure

---
+++ EasySaveRemote/Model/Client.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveRemote.Model
+{
+    class Client
+    {
+        /*
+      static void Main(string[] args)
+      {
+          Socket socket = Connect("127.0.0.1", 9050);
+          ListenNetwork(socket);
+          Disconnect(socket);
+      }
+      */
+
+        public string Name { get; set; }
+        public string SourceDirectory { get; set; }
+        public BackupType BackupType { get; set; }
+        public BackupStateEnum State { get; set; }
+        public int Progression { get; set; }
+        public bool ForceProgress { get; set; }
+
+        // Properties needeed to fill up the progression attribute
+        public int TotalFilesToCopy { get; set; }
+        public long TotalFilesSize { get; set; }
+        public int NbFilesLeftToDo { get; set; }
+
+
+        private static Socket Connect(string server, int port)
+        {
+            // create the socket
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse(server), port);
+
+            // Connect to the remote endpoint
+            socket.Connect(endpoint);
+
+            return socket;
+        }
+
+        private static int ListenNetwork(Socket client)
+        {
+            try
+            {
+
+                while (true)
+                {
+                    string read = (string)Console.ReadLine();
+                    byte[] bytes = new byte[256];
+
+
+                    byte[] readBytes = Encoding.UTF8.GetBytes(read);
+                    // Blocks until send returns.
+                    int byteCount = client.Send(readBytes, 0, readBytes.Length, SocketFlags.None);
+
+                    // Get reply from the server.
+                    byteCount = client.Receive(bytes, 0, bytes.Length, SocketFlags.None);
+                    string receivedString = Encoding.UTF8.GetString(bytes, 0, byteCount);
+
+                }
+
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("{0} Error code: {1}.", e.Message, e.ErrorCode);
+                return e.ErrorCode;
+            }
+
+        }
+
+        private static void Disconnect(Socket socket)
+        {
+            try
+            {
+                socket.Shutdown(SocketShutdown.Both);
+            }
+            finally
+            {
+                socket.Close();
+            }
+
+        }
+
+        public void SetProgression()
+        {
+            this.Progression = this.TotalFilesToCopy > 0 ? Convert.ToInt32((this.TotalFilesToCopy - this.NbFilesLeftToDo) * 100.0 / this.TotalFilesToCopy) : (ForceProgress ? 100 : 0);
+        }
+    }
+}
---
+++ EasySaveRemote/VewModel/InfoBackupViewModel.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasySaveRemote.VewModel
+{
+    class InfoBackupViewModel
+    {
+        /*
+        public string TLabelBackupName { get; set; }
+        public string TLabelSourceDirectory { get; set; }
+        public string TLabelTargetDirectory { get; set; }
+        public string TLabelBackupType { get; set; }
+        public BackupWork BackupWorkSelected { get; set; }
+
+        private string _name;
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                if (EditBackupCommand != null)
+                    EditBackupCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public string sourceDirectory { get; set; }
+        public string targetDirectory { get; set; }
+        public string backupType { get; set; }
+        */
+        private int _progress = 0;
+        public int Progress
+        {
+            get
+            {
+                return _progress;
+            }
+            set
+            {
+                _progress = value;
+                //OnPropertyChanged(nameof(Progress));
+            }
+        }
+        /*
+        private string _progressColor = "#198754";
+        public string ProgressColor
+        {
+            get
+            {
+                return _progressColor;
+            }
+            set
+            {
+                _progressColor = value;
+                OnPropertyChanged(nameof(ProgressColor));
+            }
+        }
+
+        public EditBackupCommand EditBackupCommand { get; private set; }
+        public ICommand DeleteBackupCommand { get; private set; }
+        public ICommand ExecuteBackupCommand { get; private set; }
+        public ICommand PauseBackupCommand { get; private set; }
+        public ICommand InterruptBackupCommand { get; private set; }
+
+        public InfoBackupViewModel(BackupWork BackupWorkSelected)
+        {
+            this.BackupWorkSelected = BackupWorkSelected;
+
+            BackupWorkSelected.Subscribe(this);
+
+            name = BackupWorkSelected.name;
+            sourceDirectory = BackupWorkSelected.sourceDirectory;
+            targetDirectory = BackupWorkSelected.targetDirectory;
+            backupType = BackupWorkSelected.backupType.ToString();
+
+            EditBackupCommand = new EditBackupCommand(this);
+            DeleteBackupCommand = new DeleteBackupCommand(this);
+            ExecuteBackupCommand = new ExecuteBackupCommand(this);
+            PauseBackupCommand = new PauseBackupCommand(this);
+            InterruptBackupCommand = new InterruptBackupCommand(this);
+        }
+
+        public override void SetTranslation()
+        {
+            TLabelBackupName = Translate("label_backup_name");
+            TLabelSourceDirectory = Translate("label_source_directory");
+            TLabelTargetDirectory = Translate("label_target_directory");
+            TLabelBackupType = Translate("label_backup_type");
+        }
+
+        public void OnCompleted()
+        { }
+
+        public void OnError(Exception error)
+        { }
+
+        public void OnNext(BackupState value)
+        {
+            Progress = value.Progression;
+            if (value.State == "PAUSE")
+            {
+                ProgressColor = "#ffc107";
+            }
+            else if (value.State == "INTERRUPTED")
+            {
+                ProgressColor = "#dc3545";
+            }
+            else
+            {
+                ProgressColor = "#198754";
+            }
+        }*/
+    }
+}
